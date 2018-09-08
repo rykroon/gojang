348206e1db0d1a8449aade9a81ebd05092fa253c
Ryan <rynkrn321@gmail.com>
2018-09-08 18:02:54 -0400

    changes to Manager

--- models/manager.go
+++ models/manager.go
@@ -1,27 +1,30 @@
 package models
 
-import ()
+import ("fmt")
 
 type Manager struct {
 	model *Model
+	//queryset QuerySet
 }
 
 func (m Manager) All() QuerySet {
+	fmt.Println(m.model)
 	qs := QuerySet{}
+	qs.model = m.model
 	qs.from = m.model.dbTable
 	qs.select_ = "*"
 	qs.Query = qs.buildQuery()
 	return qs
 }
 
-func (m Manager) Filter(fieldLookup string, value interface{}) QuerySet {
+func (m Manager) Filter(l lookup) QuerySet {
 	qs := m.All()
-	qs = qs.Filter(fieldLookup, value)
+	qs = qs.Filter(l)
 	return qs
 }
 
-func (m Manager) Exclude(fieldLookup string, value interface{}) QuerySet {
+func (m Manager) Exclude(l lookup) QuerySet {
 	qs := m.All()
-	qs = qs.Exclude(fieldLookup, value)
+	qs = qs.Exclude(l)
 	return qs
 }
